480dd0451172c2c020ad3f610d0d1ba07842bd3c
AlexCS04 <alexcanosala@gmail.com>
2025-07-08 18:21:47 +0200

    Update BossBehaviour.cs

--- tfg/Assets/scripts/Enemies/Boss/BossBehaviour.cs
+++ tfg/Assets/scripts/Enemies/Boss/BossBehaviour.cs
@@ -93,7 +93,8 @@ public class BossBehaviour : MonoBehaviour
 
         float progre = lvl * 0.5f;
         int bonus = lvl / 10 * 5;
-        float aditivo = 1 + (progre + bonus) / 12;
+        int extra = lvl / 100 * 18;
+        float aditivo = 1 + (progre + bonus+extra) / 12;
 
         health = transform.GetComponent<Health>();
 
